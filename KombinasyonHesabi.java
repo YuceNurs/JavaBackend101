3acc8b6b9d1655ce0c5e91a3cd6a28ec9b59a066
Nur Yuce <79469109+YuceNurs@users.noreply.github.com>
2021-10-09 16:27:42 +0300

    Add files via upload

---
+++ KombinasyonHesabi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class KombinasyonHesabi {
+
+	public static void main(String[] args) {
+		
+		int num1,num2, total1=1,total2=1,total3=1;;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Birinci Sayýyý Giriniz: ");
+		num1 = input.nextInt();
+		
+		for (int i= 1 ; i <= num1; i++) {
+			
+			total1 = total1 * i;
+		}
+		
+		System.out.println("Ýkinci Sayýyý Giriniz: ");
+		num2 = input.nextInt();
+		
+		for (int i= 1 ; i <= num2; i++) {
+			
+			total2 = total2 * i;
+		}
+		
+		for(int i = 1 ; i <= num1-num2; i++) {
+			total3 = total3 * i;
+		}
+		
+		int result = (total1) / (total2 *total3);
+		
+		System.out.println("Kombinasyon Hesaplandý: "+result);
+		
+
+	}
+
+}
