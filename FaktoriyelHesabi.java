927612cef3326ebec6333ddb1ac736c4c8c0458a
Nur Yuce <79469109+YuceNurs@users.noreply.github.com>
2021-10-09 16:24:15 +0300

    Update FaktoriyelHesabi.java

--- FaktoriyelHesabi.java
+++ FaktoriyelHesabi.java
@@ -4,11 +4,11 @@ public class FaktoriyelHesabi {
 	public static void main(String[] args) {
 		int k, total=1;
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Faktöriyel hesabý için sayi giriniz:");
+		System.out.print("FaktÃ¶riyel hesabi iÃ§in sayi giriniz:");
 		k = sc.nextInt();
 		
 		for(int i=1 ; i<=k; i++) {
-			System.out.println("dizin:"+" "+i);
+			System.out.println(i);
 			total = total*i;
 		}
 		
